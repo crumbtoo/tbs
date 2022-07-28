526fa1af0b42f3249472150b81036fe14d31804b
crumbtoo <crumb@duck.com>
2022-07-27 18:43:41 -0600

    add tbs types to test.c

--- test.c
+++ test.c
@@ -26,13 +26,33 @@ int main()
 	printf("__int128 : "TBS_WS_F"\n", sizeof(long double));
 	puts("------------------------------");
 
-	printf("u8   : "TBS_WS_F"\n", sizeof(u8));
-	printf("u16  : "TBS_WS_F"\n", sizeof(u16));
-	printf("u32  : "TBS_WS_F"\n", sizeof(u32));
+	printf("u8   : "TBS_WS_F"\n", sizeof(u8) * 8);
+	printf("u16  : "TBS_WS_F"\n", sizeof(u16) * 8);
+	printf("u32  : "TBS_WS_F"\n", sizeof(u32) * 8);
 #ifdef TBS_HAS_64
-	printf("u64  : "TBS_WS_F"\n", sizeof(u8));
+	printf("u64  : "TBS_WS_F"\n", sizeof(u64) * 8);
 #endif
 #ifdef TBS_HAS_128
-	printf("u128 : "TBS_WS_F"\n", sizeof(u128));
+	printf("u128 : "TBS_WS_F"\n", sizeof(u128) * 8);
 #endif
+
+	printf("i8   : "TBS_WS_F"\n", sizeof(i8) * 8);
+	printf("i16  : "TBS_WS_F"\n", sizeof(i16) * 8);
+	printf("i32  : "TBS_WS_F"\n", sizeof(i32) * 8);
+#ifdef TBS_HAS_64
+	printf("i64  : "TBS_WS_F"\n", sizeof(i64) * 8);
+#endif
+#ifdef TBS_HAS_128
+	printf("i128 : "TBS_WS_F"\n", sizeof(i128) * 8);
+#endif
+
+	printf("f32  : "TBS_WS_F"\n", sizeof(f32) * 8);
+	printf("f64  : "TBS_WS_F"\n", sizeof(f64) * 8);
+#if(TBS_LARGE_FLOAT_SIZE == 96)
+	printf("f96  : "TBS_WS_F"\n", sizeof(f96) * 8);
+#endif
+#if(TBS_LARGE_FLOAT_SIZE == 128)
+	printf("f128 : "TBS_WS_F"\n", sizeof(f128) * 8);
+#endif
+	printf("f64p : "TBS_WS_F"\n", sizeof(f64plus) * 8);
 }
